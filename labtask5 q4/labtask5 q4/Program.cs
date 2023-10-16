44957d105824ba35379655041feed26d19a34986
Abdullah Mushtaq Shah <213031@std.aumc.edu.pk>
2023-10-16 12:03:15 +0500

    Lab task 5

---
+++ labtask5 q4/labtask5 q4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Initialize the responses using random data (you can replace this with actual data if you have it)
+        Random rand = new Random();
+        int[] responses = new int[40];
+        for (int i = 0; i < responses.Length; i++)
+        {
+            responses[i] = rand.Next(1, 6);  // Random number between 1 and 5 (inclusive)
+        }
+
+        // Array to store the frequency of each response
+        int[] frequency = new int[5];
+
+        // Count the frequency of each response
+        foreach (int response in responses)
+        {
+            frequency[response - 1]++;
+        }
+
+        // Display the frequency summary
+        for (int i = 0; i < frequency.Length; i++)
+        {
+            Console.WriteLine($"Response {i + 1} given by: {frequency[i]} people");
+        }
+    }
+}
